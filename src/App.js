26456a748a53f96cc1475c6a9de7249e468755c3
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-10 19:53:13 +0430

    Routes established

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React, { useEffect } from 'react';
+import {
+  Routes, Route, useNavigate, useLocation,
+} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import countryInformation from './api';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
+  const currentLocation = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  console.log(typeof (process.env.REACT_APP_API_KEY));
-  const dataApi = dispatch(countryInformation('www.fac'));
-  console.log(dataApi);
+
+  // default view
+  useEffect(() => {
+    if (currentLocation.pathname !== '/') {
+      navigate('/');
+    }
+    dispatch(countryInformation('www.fac'));
+  }, []);
+
   return (
-    <h2>coming soon.</h2>
+    <>
+      <Navigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
@@ -7,6 +8,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
