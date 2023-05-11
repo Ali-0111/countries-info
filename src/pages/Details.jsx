a9c0a1b0b046cf7a2e65a6ea2a4ef8077cd6eb7a
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-11 12:07:08 +0430

    Details page rendering codes

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,55 @@
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 function Details() {
+  const navigate = useNavigate();
+
+  function homeNavigatorHandler() {
+    navigate('/');
+  }
+
+  const { countryByNameReport, isSearching } = useSelector((state) => state);
+
+  if (isSearching) {
+    return (<p>Searching...</p>);
+  }
+
   return (
-    <h2>Details Section</h2>
+    <section className="details-container">
+      <div className="searched-country-container">
+        <button
+          className="home-navigator"
+          type="button"
+          onClick={homeNavigatorHandler}
+        >
+          Home
+        </button>
+        {
+          !countryByNameReport[0]
+            ? <p>Not Matched!</p>
+            : (
+              <>
+                <h3>
+                  Country-Name:
+                  {countryByNameReport[0].name}
+                </h3>
+                <p>
+                  Capital-Name:
+                  {countryByNameReport[0].capital}
+                </p>
+                <p>
+                  Capital-Region:
+                  {countryByNameReport[0].region}
+                </p>
+                <p>
+                  Currency:
+                  {countryByNameReport[0].currency.name}
+                </p>
+              </>
+            )
+        }
+      </div>
+    </section>
   );
 }
 
