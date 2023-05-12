0441dd13068efe5ba235832ff4a3a8e7b7233f61
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-12 18:35:26 +0430

    Mocks and tests added for store

---
+++ src/tests/Mocks/mockApi.js
@@ -0,0 +1,10 @@
+function MockApi() {
+  const response = [
+    { name: 'Afghanistan', capital: 'Kabul' },
+    { name: 'Pakistan', capital: 'Islam Abad' },
+    { name: 'Iran', capital: 'Tehran' },
+  ];
+  return response;
+}
+
+export default MockApi;
